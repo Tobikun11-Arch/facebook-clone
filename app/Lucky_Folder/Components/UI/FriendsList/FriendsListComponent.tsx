f33b50983b69ebe4e32f6a744ad7ce2ac8b70479
Lucky Estrada <estrada.lnp@gmail.com>
2024-09-30 23:03:28 +0800

    Minor changes to the Nnavbar and the header component
    Improved responsiveness of the navbar and header componnents

--- app/Lucky_Folder/Components/UI/FriendsList/FriendsListComponent.tsx
+++ app/Lucky_Folder/Components/UI/FriendsList/FriendsListComponent.tsx
@@ -3,12 +3,7 @@ import FriendComponent from './FriendComponent'
 
 export default function FriendsListComponent() {
   return (
-    <div className='mx-5 my-2 pt-5'>
-        <div className='flex justify-between'>
-            <p className='font-bold'>Friend requests (16)</p>
-            <p className='text-blue-500'>See all</p>
-        </div>
-
+    <div className='mx-5'>
         <div>
             <FriendComponent></FriendComponent>
             <FriendComponent></FriendComponent>
